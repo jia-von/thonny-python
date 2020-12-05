25eaab7b997f8946a28e330534de5bb671846454
jia-von <jiavon@ualberta.ca>
2020-12-05 14:40:16 -0700

    completed drawing of snowflake using turtle library

--- Turtle Snowflakes.py
+++ Turtle Snowflakes.py
@@ -1,3 +1,9 @@
 import turtle
 pat = turtle.Turtle()
-pat.forward(100)
+for i in range (10):
+    for i in range(2):
+        pat.forward(100)
+        pat.right(60)
+        pat.forward(100)
+        pat.right(120)
+    pat.right(36)
